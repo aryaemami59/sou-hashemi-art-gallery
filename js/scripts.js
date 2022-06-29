702e90a705f2449d04723283e91804fd12253440
Arya Emami <aryaemami59@yahoo.com>
2022-06-28 22:01:17 -0500

    fixed modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 const darkModeButton = document.querySelector(".js-dark-mode");
 const topNavBar = document.querySelector(".js-navbar");
+const carouselInner = [...document.querySelectorAll(".carousel-inner")];
+const modalContent = document.querySelector(".modal-content");
+const modalImage = modalContent?.querySelector("img");
+carouselInner.forEach(e => {
+  e.addEventListener("click", () => {
+    const activeImage = e.querySelector(".carousel-item.active > img");
+    modalImage.src = activeImage.src;
+  });
+});
 darkModeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   darkModeButton.classList.toggle("text-dark");
