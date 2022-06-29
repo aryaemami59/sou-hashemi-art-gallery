196987f06bf3dc942e0136ab00bd8095cd2064b3
Arya Emami <aryaemami59@yahoo.com>
2022-06-28 23:15:09 -0500

    testing more images

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ const topNavBar = document.querySelector(".js-navbar");
 const carouselInner = [...document.querySelectorAll(".carousel-inner")];
 const modalContent = document.querySelector(".modal-content");
 const modalImage = modalContent?.querySelector("img");
+const watercolorCarouselDiv = document.querySelector(
+  ".watercolor-carousel-div"
+);
+const allegoricalCarouselDiv = document.querySelector(
+  ".allegorical-carousel-div"
+);
 carouselInner.forEach(e => {
   e.addEventListener("click", () => {
     const activeImage = e.querySelector(".carousel-item.active > img");
@@ -15,3 +21,6 @@ darkModeButton.addEventListener("click", () => {
   topNavBar.classList.toggle("navbar-light");
   topNavBar.classList.toggle("navbar-dark");
 });
+
+// const r = new FileReader();
+// r.readAsText(blob);
