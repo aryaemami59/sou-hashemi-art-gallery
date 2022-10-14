1f9d06467d7bc689799ed66a07ee3c02766541d2
Arya Emami <aryaemami59@yahoo.com>
2022-10-14 18:46:31 -0500

    added logo

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+const fullName = document.querySelector("#fullName");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const sendMessage = document.querySelector(".send-message");
+
+// const sendEmail = () => {
+//   Email.send({
+//     Host: "smtp.mailtrap.io",
+//     Username: "aryaemami59@yahoo.com",
+//     Password: "@Shakiba1994",
+//     To: "aryaemami187@gmail.com",
+//     From: email.value,
+//     Subject: "Test email",
+//     Body: `Name: ${fullName.value}\nEmail: ${email.value}\nMessage: ${message.value}`,
+//   }).then(message => alert(message));
+// };
+
+// sendMessage.addEventListener("click", sendEmail);
